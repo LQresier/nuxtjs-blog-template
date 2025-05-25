91d0a2fab88474112607dd90460f6a4b327e36a4
lqresier <qiujj9719@foxmail.com>
2025-05-26 00:02:46 +0800

    cloudflare部署

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
   modules: [
     "@nuxt/content",
     "@nuxt/eslint",
